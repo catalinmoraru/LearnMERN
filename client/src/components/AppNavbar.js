01a86072d159032cec8ec8807d6aaede7ff8bfcd
Catalin Moraru <catalin.moraru@visma.com>
2018-11-13 11:39:26 +0200

    seed app

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+
+  // Optional -> put constructor 3 - 18:18
+  state = {
+    isOpen: false
+  };
+
+  // don't have to bind this to arrow functions
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/catalinmoraru/LearnMERN.git">
+                    Github Repo
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://cata:cata123@ds063929.mlab.com:63929/learn_mern_tripletex'
+};
