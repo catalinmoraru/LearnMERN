f0f52510e54ca9d456f6b5ae140a0fb53c4c46e5
Catalin Moraru <catalin.moraru@visma.com>
2018-11-13 14:26:51 +0200

    fixed a login  bug

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -4,47 +4,57 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { connect } from 'react-redux';
 import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
+import {loginSucessful} from "./Login";
 
 class ShoppingList extends Component {
-  componentDidMount() {
-    this.props.getItems();
-  }
+	componentDidMount() {
+		this.props.getItems();
+	}
 
-  onDeleteClick = id => {
-    this.props.deleteItem(id);
-  };
+	onDeleteClick = id => {
+		this.props.deleteItem(id);
+	};
 
-  render() {
-    const { items } = this.props.item;
-    const loginSuccessful  = this.props.loginSuccessful;
-	alert(loginSuccessful);
+	render() {
+		const {items} = this.props.item;
+		const loginSuccessful = this.props.loginSuccessful;
+		// alert(loginSuccessful);
 
-    return (
+		if (loginSuccessful) {
 
 
-		<Container>
-        <ListGroup>
-          <TransitionGroup className="shopping-list">
-            {items.map(({ _id, name }) => (
-              <CSSTransition key={_id} timeout={500} classNames="fade">
-                <ListGroupItem>
-                  <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    onClick={this.onDeleteClick.bind(this, _id)}
-                  >
-                    &times;
-                  </Button>
-                  {name}
-                </ListGroupItem>
-              </CSSTransition>
-            ))}
-          </TransitionGroup>
-        </ListGroup>
-      </Container>
+			return (
 
-    );
+
+				<Container>
+					<ListGroup>
+						<TransitionGroup className="shopping-list">
+							{items.map(({_id, name}) => (
+								<CSSTransition key={_id} timeout={500} classNames="fade">
+									<ListGroupItem>
+										<Button
+											className="remove-btn"
+											color="danger"
+											size="sm"
+											onClick={this.onDeleteClick.bind(this, _id)}
+										>
+											&times;
+										</Button>
+										{name}
+									</ListGroupItem>
+								</CSSTransition>
+							))}
+						</TransitionGroup>
+					</ListGroup>
+				</Container>
+
+			);
+
+	} else {
+		return(
+			<div>Please login</div>
+		)
+	}
   }
 }
 
