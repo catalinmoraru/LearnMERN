ce7faa6d4744a5da382f1fa921671c92f075f152
Catalin Moraru <catalin.moraru@visma.com>
2018-11-13 18:14:17 +0200

    added image

--- client/src/components/Gold.js
+++ client/src/components/Gold.js
@@ -16,10 +16,13 @@ class Gold extends Component {
 		if (loginSuccessful) {
 			return (
 				<div >
-					<span className="col-md-4">
+					<span className="col-md-6">
 					<Alert color="warning">
-						You have : {this.props.gold} gold
+						{/*<img  src="https://i.pinimg.com/474x/c4/ac/e9/c4ace97a0a140586a3eacee6dd2bd247.jpg"></img>*/}
+						<img  src="https://2images.cgames.de/images/gamestar/204/witcher-3-daumen-hoch_2792771.jpg"></img>
+						<h2> You have : {this.props.gold} gold </h2>
 						<img  src="https://i.imgur.com/ux6Apdn.png"></img>
+
 					</Alert>
 
 					</span>
